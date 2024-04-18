7b505fa96b3e0dd17d6ba1bc1fb5e5e2bd385fbf
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-17 21:52:37 -0500

    Update with latest changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
+// next.config.mjs
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        child_process: false,
+        path: false,
+        os: false,
+        vm: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        zlib: false,
+        dns: false,
+      };
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- pages/api/mongoData.js
+++ pages/api/mongoData.js
@@ -1,7 +1,8 @@
+// pages/api/mongoData.js
 import dbConnect from '../../utils/dbConnect';
 
 export default async function handler(req, res) {
   const { db } = await dbConnect();
   const mongoData = await db.collection('Lessons').find({}).toArray();
-  res.status(200).json(mongoDatav);
+  res.status(200).json(mongoData);  // Corrected here
 }
