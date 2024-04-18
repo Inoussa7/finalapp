1bb47ac06e9446e1c13ead6bbbd80f118b3a8b0e
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-17 21:38:13 -0500

    Update with latest changes

--- pages/api/mongoData.js
+++ pages/api/mongoData.js
@@ -3,5 +3,5 @@ import dbConnect from '../../utils/dbConnect';
 export default async function handler(req, res) {
   const { db } = await dbConnect();
   const mongoData = await db.collection('Lessons').find({}).toArray();
-  res.status(200).json(mongoData);
+  res.status(200).json(mongoDatav);
 }
