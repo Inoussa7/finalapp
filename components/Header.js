89aa4a881f424b92802b2bf2982caeab7cc1e397
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-17 20:25:31 -0500

    Update with latest changes

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,8 @@ const Header = () => {
             <div className={styles.container}>
                 <h1>Welcome to TEAC 882B</h1>
                 <h2>Database and Interactive Web Development</h2>
-                <h2>Trilingual Learning App</h2>
+                <h2>Language Learning App</h2>
+                <h2>Final Project</h2>
             </div>
         </header>
     );
