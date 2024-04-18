39587663a8626a43985acfdabb3a9b06070a9792
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-17 22:35:59 -0500

    Update with latest changes

--- pages/index.js
+++ pages/index.js
@@ -36,15 +36,14 @@ const HomePage = () => {
       const res = await fetch('/api/mongoData');
       const mongoData = await res.json();
       setMongoData(mongoData);
-     // Debug code
-    console.log('MongoDB data:', mongoData);
-    mongoData.forEach(doc => {
-      for (let key in doc) {
-        if (typeof doc[key] === 'string' && doc[key].startsWith('Hello')) {
-          console.log(`Found a string that starts with 'Hello' in MongoDB data: ${doc[key]}`);
-        }
-      }
-    });
+     /console.log('MongoDB data:', mongoData);
+     mongoData.forEach(doc => {
+       for (let key in doc) {
+         if (typeof doc[key] === 'string' && doc[key].startsWith('Hello')) {
+           console.log(`Found a string that starts with 'Hello' in MongoDB data: ${doc[key]}`);
+         }
+       }
+     });
     } catch (error) {
       console.error('Error fetching MongoDB data:', error);
     }
