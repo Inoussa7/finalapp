ac00ee07e2ddb79f893064f1c2c5ada0b77ede31
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-17 19:19:44 -0500

    Update with latest changes

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,72 @@
-import Page from '../app/page';
-import { sql } from '@vercel/postgres';
+// pages/index.js
 
-export async function getServerSideProps() {
-    const returned = await sql`SELECT * FROM language_table`;
-    return {
-        props: { users: returned.rows },
-    };
-}
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
+import SignUpForm from '../components/SignUpForm';
+import LoginForm from '../components/LoginForm';
+import styles from "./page.module.css";
+import { sql } from '@vercel/postgres'; 
 
-export default function HomePage(props) {
-    return <Page {...props} />;
-}
+const HomePage = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const returned = await sql`SELECT * FROM language_table`;
+      setUsers(returned.rows);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs only once after the component mounts
+
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  const handleSignUpClick = (event) => {
+    event.preventDefault();
+    setShowSignUp(true);
+    setShowSignIn(false);
+  };
+
+  const handleSignInClick = (event) => {
+    event.preventDefault();
+    setShowSignIn(true);
+    setShowSignUp(false);
+  };
+
+  return (
+    <>
+      <Header />
+      <Navigation />
+      {showSignUp && <SignUpForm />}
+      {showSignIn && <LoginForm />}
+
+      <br />
+      <table>
+        <thead>
+          <tr>
+            {users.length > 0 && Object.keys(users[0]).map((key) => (
+              <th key={key}>{key}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((item) => (
+            <tr key={item.id}>
+              {Object.values(item).map((value, index) => (
+                <td key={index}>{value instanceof Date ? value.toISOString() : value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default HomePage;
