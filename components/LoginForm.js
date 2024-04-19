43d37d315ec5d3fcd060470e2086f09589eb9c2c
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-18 19:45:10 -0500

    Update with latest changes

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -31,13 +31,13 @@ const LoginForm = () => {
         <form onSubmit={handleSubmit}>
             <label>
                 Username:
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
             </label>
             <label>
                 Password:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
             </label>
-            <button type="submit">Login</button>
+            <input type="submit" value="Submit" />
         </form>
     );
 };
